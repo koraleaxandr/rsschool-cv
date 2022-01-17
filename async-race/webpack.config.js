bd91cd5e098a071d78fa36fc7b3932e6202f121d
koraleaxandr <koraleaxandr@gmail.com>
2022-01-17 09:58:12 +0300

    init: create app folder

---
+++ async-race/webpack.config.js
@@ -0,0 +1,23 @@
+/* eslint-disable no-undef */
+/* eslint-disable @typescript-eslint/no-var-requires */
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    entry: './app/index.ts',
+    module: {
+    rules: [
+     { test: /\.svg$/, use: 'svg-inline-loader' },
+      { test: /\.css$/, use: [ 'style-loader', 'css-loader' ] },
+      { test: /\.(js)$/, use: 'babel-loader' }
+    ]
+  },
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'index_bundle.js'
+  },
+  plugins: [
+    new HtmlWebpackPlugin(),    
+  ],
+  mode: process.env.NODE_ENV === 'production' ? 'production' : 'development'
+}
